b1b2faa05ee0c4bc2a618cdc34b5cdac76bf3584
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-09 11:23:47 +0000

    added support for basic js conditions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_hijax/trunk@56130 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScript/hijax.js
+++ Resources/Public/JavaScript/hijax.js
@@ -50,65 +50,82 @@
 		if (elements && elements.length > 0) {
 			var requests = [];
 			$.each(elements, function(i, element) {
-				var el = {
-					id: $(element).attr('id'),
-					extension: $(element).attr('data-hijax-extension'),
-					plugin: $(element).attr('data-hijax-plugin'),
-					controller: $(element).attr('data-hijax-controller'),
-					action: $(element).attr('data-hijax-action'),
-					arguments: $(element).attr('data-hijax-arguments'),
-					settingsHash: $(element).attr('data-hijax-settings')
-				};
-				var loader = $(element).find('> .'+EXTBASE_HIJAX.loadingClass);
-				if (!loader.data('targetOpacity')) {
-					loader.data('targetOpacity', loader.css('opacity'));
-					loader.css('opacity', 0);
+				var el = $(element);
+				if (el.attr('data-hijax-element-type')=='conditional') {
+					try {
+						val = eval(el.attr('data-hijax-condition'));
+						if (!val) {
+							el.css('display', 'none');
+						} else {
+							el.find('> .hijax-content').css('visibility', 'visible');
+						}
+					} catch (err) {
+						el.css('display', 'none');
+					}
+				} else {
+						// ajax request
+					var el = {
+						id: $(element).attr('id'),
+						extension: $(element).attr('data-hijax-extension'),
+						plugin: $(element).attr('data-hijax-plugin'),
+						controller: $(element).attr('data-hijax-controller'),
+						action: $(element).attr('data-hijax-action'),
+						arguments: $(element).attr('data-hijax-arguments'),
+						settingsHash: $(element).attr('data-hijax-settings')
+					};
+					var loader = $(element).find('> .'+EXTBASE_HIJAX.loadingClass);
+					if (!loader.data('targetOpacity')) {
+						loader.data('targetOpacity', loader.css('opacity'));
+						loader.css('opacity', 0);
+					}
+					loader.show();
+					loader.stop().animate({
+						opacity: loader.data('targetOpacity')
+					}, 500, function() {
+							// Animation complete.
+					});
+					requests.push(el);
 				}
-				loader.show();
-				loader.stop().animate({
-					opacity: loader.data('targetOpacity')
-				}, 500, function() {
-						// Animation complete.
-				});
-				requests.push(el);
 			});
 			
-			var ajaxRequest = $.ajax({
-				url: EXTBASE_HIJAX.url,
-				type: "POST",
-				//crossDomain: true,
-				data: {r: requests},
-				dataType: "jsonp",
-				success: function(data) { 
-					$.each(data, function(i, r) {
-						var element = $('#'+r['id']);
-						if (element) {
-							var loader = element.find('> .'+EXTBASE_HIJAX.loadingClass);
-							var content = element.find('> .'+EXTBASE_HIJAX.contentClass);
-							
-							element.removeClass(EXTBASE_HIJAX.fallbackClass);
+			if (requests.length>0) {
+				var ajaxRequest = $.ajax({
+					url: EXTBASE_HIJAX.url,
+					type: "POST",
+					//crossDomain: true,
+					data: {r: requests},
+					dataType: "jsonp",
+					success: function(data) { 
+						$.each(data, function(i, r) {
+							var element = $('#'+r['id']);
+							if (element) {
+								var loader = element.find('> .'+EXTBASE_HIJAX.loadingClass);
+								var content = element.find('> .'+EXTBASE_HIJAX.contentClass);
+								
+								element.removeClass(EXTBASE_HIJAX.fallbackClass);
 
-							content.html(r['response']).css('height', content.height());
-							
-							if (!loader.data('targetOpacity')) {
-								loader.data('targetOpacity', loader.css('opacity'));
+								content.html(r['response']).css('height', content.height());
+								
+								if (!loader.data('targetOpacity')) {
+									loader.data('targetOpacity', loader.css('opacity'));
+								}
+								loader.stop().animate({
+									opacity: 0
+								}, 500, function() {
+										// Animation complete.
+									loader.hide();
+								});
+								
+								element.stop().animate({
+									height: content.outerHeight()
+								}, 500, function() {
+										// Animation complete.
+								});
 							}
-							loader.stop().animate({
-								opacity: 0
-							}, 500, function() {
-									// Animation complete.
-								loader.hide();
-							});
-							
-							element.stop().animate({
-								height: content.outerHeight()
-							}, 500, function() {
-									// Animation complete.
-							});
-						}
-					});
-				}
-			});			
+						});
+					}
+				});	
+			}
 		}
 	};
 	
