ea9d12fe988e80b35fb8f4f3abcb2a06a615b094
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-10 09:35:58 +0000

    updated version number
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_hijax/trunk@56165 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "extbase_hijax".
 #
-# Auto generated 22-12-2011 11:23
+# Auto generated 10-01-2012 10:35
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.1.dev',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'suggests' => array(
 	),
 	'conflicts' => '',
-	'_md5_values_when_last_written' => 'a:15:{s:12:"ext_icon.gif";s:4:"b280";s:17:"ext_localconf.php";s:4:"22e5";s:14:"ext_tables.php";s:4:"1dee";s:35:"Classes/Configuration/Extension.php";s:4:"b390";s:44:"Classes/Configuration/ExtensionInterface.php";s:4:"5d01";s:25:"Classes/Tslib/FE/Hook.php";s:4:"6f10";s:35:"Classes/Utility/Ajax/Dispatcher.php";s:4:"6e7c";s:38:"Configuration/TypoScript/constants.txt";s:4:"58af";s:34:"Configuration/TypoScript/setup.txt";s:4:"dd3c";s:36:"Resources/Private/Eid/dispatcher.php";s:4:"fa09";s:40:"Resources/Private/Language/locallang.xml";s:4:"cbd6";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"3159";s:30:"Resources/Public/CSS/hijax.css";s:4:"c5f9";s:34:"Resources/Public/Images/loader.gif";s:4:"f53b";s:36:"Resources/Public/JavaScript/hijax.js";s:4:"d453";}',
+	'_md5_values_when_last_written' => 'a:19:{s:12:"ext_icon.gif";s:4:"b280";s:17:"ext_localconf.php";s:4:"2deb";s:14:"ext_tables.php";s:4:"1dee";s:35:"Classes/Configuration/Extension.php";s:4:"846f";s:44:"Classes/Configuration/ExtensionInterface.php";s:4:"c4f1";s:25:"Classes/Tslib/FE/Hook.php";s:4:"9e0e";s:35:"Classes/Utility/Ajax/Dispatcher.php";s:4:"41e4";s:38:"Classes/ViewHelpers/ElseViewHelper.php";s:4:"b415";s:38:"Classes/ViewHelpers/FormViewHelper.php";s:4:"3b0a";s:36:"Classes/ViewHelpers/IfViewHelper.php";s:4:"c7cd";s:38:"Classes/ViewHelpers/ThenViewHelper.php";s:4:"47fd";s:38:"Configuration/TypoScript/constants.txt";s:4:"58af";s:34:"Configuration/TypoScript/setup.txt";s:4:"4582";s:36:"Resources/Private/Eid/dispatcher.php";s:4:"f083";s:40:"Resources/Private/Language/locallang.xml";s:4:"cbd6";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"3159";s:30:"Resources/Public/CSS/hijax.css";s:4:"f0f8";s:34:"Resources/Public/Images/loader.gif";s:4:"f53b";s:36:"Resources/Public/JavaScript/hijax.js";s:4:"8958";}',
 );
 
 ?>
