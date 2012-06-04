4836c2d51a92fe0e8752413f73cdd6ff93b3af70
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-04 22:38:37 +0000

    added slots dispatching on update/insert/remove actions on repositories
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_hijax/trunk@63097 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Persistence/Backend.php
+++ Classes/Persistence/Backend.php
@@ -27,12 +27,71 @@
 class Tx_ExtbaseHijax_Persistence_Backend extends Tx_Extbase_Persistence_Backend {
 
 	/**
+	 * @var Tx_Extbase_SignalSlot_Dispatcher
+	 */
+	protected $signalSlotDispatcher;
+	
+	/**
+	 * @param Tx_Extbase_SignalSlot_Dispatcher $signalSlotDispatcher
+	 */
+	public function injectSignalSlotDispatcher(Tx_Extbase_SignalSlot_Dispatcher $signalSlotDispatcher) {
+		$this->signalSlotDispatcher = $signalSlotDispatcher;
+	}
+	
+	/**
 	 * @param Tx_ExtbaseHijax_Persistence_Storage_Typo3DbBackend $storageBackend
 	 * @return void
 	 */
 	public function injectStorageBackend(Tx_ExtbaseHijax_Persistence_Storage_Typo3DbBackend $storageBackend) {
 		$this->storageBackend = $storageBackend;
 	}
+	
+	/**
+	 * Inserts an object in the storage backend
+	 *
+	 * @param Tx_Extbase_DomainObject_DomainObjectInterface $object The object to be insterted in the storage
+	 * @return void
+	 */
+	protected function insertObject(Tx_Extbase_DomainObject_DomainObjectInterface $object) {
+		parent::insertObject($object);
+		
+		if ($object->getUid() >= 1) {
+			$this->signalSlotDispatcher->dispatch('Tx_Extbase_Persistence_Backend', 'afterInsertObject', array('object' => $object));
+		}		
+	}
+	
+	/**
+	 * Updates a given object in the storage
+	 *
+	 * @param Tx_Extbase_DomainObject_DomainObjectInterface $object The object to be updated
+	 * @param array $row Row to be stored
+	 * @return bool
+	 */
+	protected function updateObject(Tx_Extbase_DomainObject_DomainObjectInterface $object, array $row) {
+		$result = parent::updateObject($object, $row);
+		
+		if ($result === TRUE) {
+			$this->signalSlotDispatcher->dispatch('Tx_Extbase_Persistence_Backend', 'afterUpdateObject', array('object' => $object));
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * Deletes an object
+	 *
+	 * @param Tx_Extbase_DomainObject_DomainObjectInterface $object The object to be removed from the storage
+	 * @param bool $markAsDeleted Wether to just flag the row deleted (default) or really delete it
+	 * @return void
+	 */
+	protected function removeObject(Tx_Extbase_DomainObject_DomainObjectInterface $object, $markAsDeleted = TRUE) {
+		// TODO: check if object is not already deleted
+		
+		parent::removeObject($object, $markAsDeleted);
+		
+		// TODO: check if object is removed indeed
+		$this->signalSlotDispatcher->dispatch('Tx_Extbase_Persistence_Backend', 'afterRemoveObject', array('object' => $object));
+	}	
 }
 
 ?>
