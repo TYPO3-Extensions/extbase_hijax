568d59fff4c21b2c87ee8179182a600d029e512d
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-10-16 08:17:56 +0000

    * fixed requirement for TYPO3 6.1.5 (for essential dots build process)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_hijax/trunk@80960 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0.dev',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '6.1.5',
+			'typo3' => '6.1.5',
 			'extbase' => '',
 			'fluid' => '',
 		),
