ad9ba1e604d3bcea391f2046e3c3ec0dfe5a42c9
edots <edots@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-09 11:58:16 +0000

    * added noAjax and forceContext parameters to h:link.action viewhelper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extbase_hijax/trunk@84194 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -1,5 +1,6 @@
 <?php
 namespace EssentialDots\ExtbaseHijax\ViewHelpers\Link;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /***************************************************************
  *  Copyright notice
@@ -42,6 +43,25 @@ class ActionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\ActionViewHelpe
 	protected $hijaxEventDispatcher;
 
 	/**
+	 * @var	\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer
+	 */
+	protected $contentObject;
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
+	 */
+	protected $configurationManager;
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager) {
+		$this->configurationManager = $configurationManager;
+		$this->contentObject = $this->configurationManager->getContentObject();
+	}
+
+	/**
 	 * Injects the event dispatcher
 	 *
 	 * @param \EssentialDots\ExtbaseHijax\Event\Dispatcher $eventDispatcher
@@ -78,78 +98,95 @@ class ActionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\ActionViewHelpe
 	 * @param string $format
 	 * @param int $pageUid
 	 * @param boolean $cachedAjaxIfPossible TRUE if the URI should be cached (with respect to non-cacheable actions)
+	 * @param boolean $forceContext TRUE if the controller/action/... should be passed
+	 * @param boolean $noAjax
 	 *
 	 * @return string
 	 */
-	public function render($action = NULL, array $arguments = array(), $controller = NULL, $extensionName = NULL, $pluginName = NULL, $format = '', $pageUid = 0, $cachedAjaxIfPossible = TRUE) {
+	public function render($action = NULL, array $arguments = array(), $controller = NULL, $extensionName = NULL, $pluginName = NULL, $format = '', $pageUid = NULL, $cachedAjaxIfPossible = TRUE, $forceContext = TRUE, $noAjax = FALSE) {
 		$request = $this->mvcDispatcher->getCurrentRequest();
 
-		/* @var $listener \EssentialDots\ExtbaseHijax\Event\Listener */
-		$listener = $this->mvcDispatcher->getCurrentListener();
-
-		if ($request) {
-			if ($action === NULL) {
-				$action = $request->getControllerActionName();
-			}
-
-			if ($controller === NULL) {
-				$controller = $request->getControllerName();
-			}
-
-			if ($extensionName === NULL) {
-				$extensionName = $request->getControllerExtensionName();
-			}
-
-			if ($pluginName === NULL && TYPO3_MODE === 'FE') {
-				$pluginName = $this->extensionService->getPluginNameByAction($extensionName, $controller, $action);
-			}
-			if ($pluginName === NULL) {
-				$pluginName = $request->getPluginName();
-			}
-
-			if ($cachedAjaxIfPossible) {
-				/* @var $cacheHash \TYPO3\CMS\Frontend\Page\CacheHashCalculator */
-				$cacheHash = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\CacheHashCalculator');
-				$chash = $cacheHash->calculateCacheHash(array(
-					'encryptionKey' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'],
-					'action' => $action,
-					'controller' => $controller,
-					'extension' => $extensionName,
-					'plugin' => $pluginName,
-					'arguments' => $this->array_map_recursive('strval', $arguments),
-					'settingsHash' => $listener->getId()
-				));
-			}
+		if ($forceContext) {
+			$requestArguments = $this->controllerContext->getRequest()->getArguments();
+			$requestArguments = array_merge($requestArguments, $this->hijaxEventDispatcher->getContextArguments());
+			$requestArguments = array_merge($requestArguments, $arguments);
+			$arguments = $requestArguments;
 		}
 
-		$additionalArguments = array();
-		$this->hA('r[0][arguments]', $arguments, $additionalArguments);
+		if ($noAjax) {
+			return parent::render($action, $arguments, $controller, $extensionName, $pluginName, $pageUid);
+		} else {
 
-		$language = intval($GLOBALS['TSFE'] ? $GLOBALS['TSFE']->sys_language_content : 0);
-		$additionalParams = "&r[0][extension]={$extensionName}&r[0][plugin]={$pluginName}&r[0][controller]={$controller}&r[0][action]={$action}&r[0][format]={$format}&r[0][settingsHash]={$listener->getId()}&eID=extbase_hijax_dispatcher&L={$language}";
+			/* @var $listener \EssentialDots\ExtbaseHijax\Event\Listener */
+			$listener = $this->mvcDispatcher->getCurrentListener();
 
-		if ($additionalArguments) {
-			$additionalParams .= '&'.implode('&', $additionalArguments);
+			if ($request) {
+				if ($action === NULL) {
+					$action = $request->getControllerActionName();
+				}
+
+				if ($controller === NULL) {
+					$controller = $request->getControllerName();
+				}
+
+				if ($extensionName === NULL) {
+					$extensionName = $request->getControllerExtensionName();
+				}
+
+				if ($pluginName === NULL && TYPO3_MODE === 'FE') {
+					$pluginName = $this->extensionService->getPluginNameByAction($extensionName, $controller, $action);
+				}
+				if ($pluginName === NULL) {
+					$pluginName = $request->getPluginName();
+				}
+
+				$cachedAjaxIfPossible = $cachedAjaxIfPossible ? $this->configurationManager->getContentObject()->getUserObjectType() != ContentObjectRenderer::OBJECTTYPE_USER_INT : false;
+
+				if ($cachedAjaxIfPossible) {
+					/* @var $cacheHash \TYPO3\CMS\Frontend\Page\CacheHashCalculator */
+					$cacheHash = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\CacheHashCalculator');
+					$chash = $cacheHash->calculateCacheHash(array(
+						'encryptionKey' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'],
+						'action' => $action,
+						'controller' => $controller,
+						'extension' => $extensionName,
+						'plugin' => $pluginName,
+						'arguments' => $this->array_map_recursive('strval', $arguments),
+						'settingsHash' => $listener->getId()
+					));
+				}
+			}
+
+			$additionalArguments = array();
+			$this->hA('r[0][arguments]', $arguments, $additionalArguments);
+
+			$language = intval($GLOBALS['TSFE'] ? $GLOBALS['TSFE']->sys_language_content : 0);
+			$additionalParams = "&r[0][extension]={$extensionName}&r[0][plugin]={$pluginName}&r[0][controller]={$controller}&r[0][action]={$action}&r[0][format]={$format}&r[0][settingsHash]={$listener->getId()}&eID=extbase_hijax_dispatcher&L={$language}";
+
+			if ($additionalArguments) {
+				$additionalParams .= '&'.implode('&', $additionalArguments);
+			}
+
+			if ($chash) {
+				$additionalParams .= '&r[0][chash]='.$chash;
+			}
+
+
+			/* @var $cObj \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer */
+			$cObj = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
+
+			$uri = $cObj->typoLink('', array(
+				'returnLast' => 'url',
+				'additionalParams' => $additionalParams,
+				'parameter' => $pageUid ? $pageUid : ($GLOBALS['TSFE'] ? $GLOBALS['TSFE']->id : 0)
+			));
+
+			$this->tag->addAttribute('href', $uri);
+			$this->tag->setContent($this->renderChildren());
+			$this->tag->forceClosingTag(TRUE);
+
+			return $this->tag->render();
 		}
-
-		if ($chash) {
-			$additionalParams .= '&r[0][chash]='.$chash;
-		}
-
-		/* @var $cObj \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer */
-		$cObj = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
-
-		$uri = $cObj->typoLink('', array(
-			'returnLast' => 'url',
-			'additionalParams' => $additionalParams,
-			'parameter' => $pageUid ? $pageUid : ($GLOBALS['TSFE'] ? $GLOBALS['TSFE']->id : 0)
-		));
-
-		$this->tag->addAttribute('href', $uri);
-		$this->tag->setContent($this->renderChildren());
-		$this->tag->forceClosingTag(TRUE);
-
-		return $this->tag->render();
 	}
 	
 	/**
